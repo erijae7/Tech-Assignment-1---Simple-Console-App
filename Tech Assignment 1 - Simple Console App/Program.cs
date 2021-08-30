fada5e1bbb73083ef860ca75871e6a3983612906
ejmeo <ejmeo@DESKTOP-N1E8MB5>
2021-08-30 12:50:36 -0400

    Add project files.

---
+++ Tech Assignment 1 - Simple Console App/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tech_Assignment_1___Simple_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display Message
+            Console.WriteLine("Welcome to Tech Assignment 1 - Simple Console. Click anykey to contiune...");  
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true); 
+
+        }
+    }
+}
